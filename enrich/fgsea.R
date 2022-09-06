eb6cbe6b33c799e1751595d9ac3df92e9d95a9c3
Geert-Jan Huizing <huizing@ens.fr>
2022-09-06 11:43:14 +0200

    custom gene sets in fgsea

--- enrich/fgsea.R
+++ enrich/fgsea.R
@@ -4,13 +4,13 @@ library(fgsea)
 library(ggplot2)
 
 # Loading a GMT file.
-pathways <- c(
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Biological_Process_2021.gmt"), # nolint
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Cellular_Component_2021.gmt"), # nolint
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Molecular_Function_2021.gmt"), # nolint
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/KEGG_2021_Human.gmt"), # nolint
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/PanglaoDB_Augmented_2021.gmt"), # nolint
-    gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/Reactome_2016.gmt") # nolint
+pathways <- list(
+    "GO_Biological_Process_2021" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Biological_Process_2021.gmt"), # nolint
+    "GO_Cellular_Component_2021" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Cellular_Component_2021.gmt"), # nolint
+    "GO_Molecular_Function_2021" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/GO_Molecular_Function_2021.gmt"), # nolint
+    "KEGG_2021_Human" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/KEGG_2021_Human.gmt"), # nolint
+    "PanglaoDB_Augmented_2021" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/PanglaoDB_Augmented_2021.gmt"), # nolint
+    "Reactome_2016" = gmtPathways("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/gmts/Reactome_2016.gmt") # nolint
 )
 
 # Initializing a dataframe with the results.
@@ -19,47 +19,57 @@ total_fgsea_res <- data.frame()
 # Loading MOFA+'s ranks.
 metagenes <- read.csv("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/mofa.rnk", header = TRUE, row.names = 1) # nolint
 
-for (col in colnames(metagenes)) {
-    # Get ranks for a given dimension.
-    ranks <- setNames(as.matrix(metagenes[col]), rownames(metagenes))
+for (source in names(pathways)) {
+    print(source)
+    for (col in colnames(metagenes)) {
+        print(col)
+        # Get ranks for a given dimension.
+        ranks <- setNames(as.matrix(metagenes[col]), rownames(metagenes))
 
-    # Run fgsea.
-    fgsea_res <- fgsea(
-        pathways = pathways,
-        stats = ranks,
-        minSize = 15,
-        maxSize = 500,
-        nperm = 1000
-    )
+        # Run fgsea.
+        fgsea_res <- fgsea(
+            pathways = pathways[source],
+            stats = ranks,
+            minSize = 15,
+            maxSize = 500,
+            nperm = 1000
+        )
 
-    # Add some metadata.
-    fgsea_res$col <- col
-    fgsea_res$method <- "mofa"
+        # Add some metadata.
+        fgsea_res$col <- col
+        fgsea_res$method <- "mofa"
+        fgsea_res$source <- source
 
-    # Add the results to the total.
-    total_fgsea_res <- rbind(total_fgsea_res, fgsea_res)
+        # Add the results to the total.
+        total_fgsea_res <- rbind(total_fgsea_res, fgsea_res)
+    }
 }
 
 # Loading Mowgli's ranks.
 metagenes <- read.csv("/users/csb/huizing/Documents/PhD/Code/mowgli_reproducibility/enrich/mowgli.rnk", header = TRUE, row.names = 1) # nolint
 
-for (col in colnames(metagenes)) {
-    # Get ranks for a given dimension.
-    ranks <- setNames(as.matrix(metagenes[col]), rownames(metagenes))
+for (source in names(pathways)) {
+    print(source)
+    for (col in colnames(metagenes)) {
+        print(col)
+        # Get ranks for a given dimension.
+        ranks <- setNames(as.matrix(metagenes[col]), rownames(metagenes))
 
-    # Running fgsea.
-    fgsea_res <- fgsea(
-        pathways = pathways,
-        stats = ranks,
-        minSize = 15,
-        maxSize = 500,
-        nperm = 1000,
-        scoreType = "pos"
-    )
-    fgsea_res$col <- col
-    fgsea_res$method <- "mowgli"
+        # Running fgsea.
+        fgsea_res <- fgsea(
+            pathways = pathways[source],
+            stats = ranks,
+            minSize = 15,
+            maxSize = 500,
+            nperm = 1000,
+            scoreType = "pos"
+        )
+        fgsea_res$col <- col
+        fgsea_res$method <- "mowgli"
+        fgsea_res$source <- source
 
-    total_fgsea_res <- rbind(total_fgsea_res, fgsea_res)
+        total_fgsea_res <- rbind(total_fgsea_res, fgsea_res)
+    }
 }
 
 # Saving results.
